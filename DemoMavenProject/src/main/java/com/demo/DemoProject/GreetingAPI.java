21f60f84623e26ffc0f0958aaa3afa10cbcc2c85
Saba Perween <sabakhanjmi0@gmail.com>
2023-08-26 22:05:40 +0530

    first commit

---
+++ DemoMavenProject/src/main/java/com/demo/DemoProject/GreetingAPI.java
@@ -0,0 +1,17 @@
+package com.demo.DemoProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingAPI {
+    @GetMapping("/birthdayGeeting")
+    public String[] getBirthdayGreeting(){
+        String[] str=new String[4];
+        str[0]="Wishing you very happy birthdays";
+        str[1]="Happy BirthDay";
+        str[2]="Many Many happy returns of the day";
+        str[3]="Let's party.....!!!!";
+        return str;
+    }
+}
