7a5d4659f3d7f266799d697ebb51df90193bf6e0
Saba Perween <sabakhanjmi0@gmail.com>
2023-08-27 20:33:19 +0530

    changed

--- src/main/java/com/demo/DemoProject/GreetingAPI.java
+++ src/main/java/com/demo/DemoProject/GreetingAPI.java
@@ -8,7 +8,7 @@ public class GreetingAPI {
     @GetMapping("/birthdayGeeting")
     public String[] getBirthdayGreeting(){
         String[] str=new String[4];
-        str[0]="Wishing you very happy birthdays";
+        str[0]="Wishing you very happy birthday";
         str[1]="Happy BirthDay";
         str[2]="Many Many happy returns of the day";
         str[3]="Let's party.....!!!!";
